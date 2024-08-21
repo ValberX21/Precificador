4f92a8e4b7be7c7b6b9b241bddc01bed82709ede
Anderson Jose Marzola <anderson.marzola@FIESP.COM.BR>
2024-08-21 17:21:41 -0300

    projeto app

---
+++ Precificador.App/FrmNovoProduto.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Precificador.App
+{
+    partial class FrmNovoProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmNovoProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 287);
+            this.Name = "FrmNovoProduto";
+            this.Text = "Cadastro de Produto";
+            this.Load += new System.EventHandler(this.FrmNovoProduto_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Precificador.App/FrmNovoProduto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Precificador.App
+{
+    public partial class FrmNovoProduto : Form
+    {
+        public FrmNovoProduto()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmNovoProduto_Load(object sender, EventArgs e)
+        {
+            CarregarCombos();
+        }
+
+        private void Salvar()
+        {
+
+        }
+    }
+}
