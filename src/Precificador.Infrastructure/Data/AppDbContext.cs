31ee81b38ed3e93c8727a798310c2a9aca30b48c
Anderson Marzola <ajmarzola@gmail.com>
2025-06-19 13:14:18 -0300

    Relacionamentos

--- src/Precificador.Infrastructure/Data/AppDbContext.cs
+++ src/Precificador.Infrastructure/Data/AppDbContext.cs
@@ -15,7 +15,76 @@ namespace Precificador.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //TODO: 19/06/2025 - Configurar entidades e relacionamentos aqui
+            modelBuilder.Entity<MateriaPrima>()
+                .HasOne(mp => mp.Grupo)
+                .WithMany(g => g.MateriasPrimas)
+                .HasForeignKey(mp => mp.GrupoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProdutoMateriaPrima>()
+                .HasOne(pmp => pmp.MateriaPrima)
+                .WithMany(mp => mp.Produtos)
+                .HasForeignKey(pmp => pmp.MateriaPrimaId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<ProdutoMateriaPrima>()
+                .HasOne(pmp => pmp.Produto)
+                .WithMany(p => p.MateriasPrimas)
+                .HasForeignKey(pmp => pmp.ProdutoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<PesquisaPreco>()
+                .HasOne(pp => pp.Produto)
+                .WithMany(p => p.Pesquisas)
+                .HasForeignKey(pp => pp.ProdutoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Produto>()
+                .HasOne(p => p.Colecao)
+                .WithMany(c => c.Produtos)
+                .HasForeignKey(p => p.ColecaoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<MateriaPrima>()
+                .HasOne(mp => mp.UnidadeMedida)
+                .WithMany()
+                .HasForeignKey(mp => mp.UnidadeMedidaId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Grupo>()
+                .Property(g => g.Nome)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<MateriaPrima>()
+                .Property(mp => mp.Nome)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            modelBuilder.Entity<Colecao>()
+                .Property(c => c.Nome)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            modelBuilder.Entity<PesquisaPreco>()
+                .Property(pp => pp.Local)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            modelBuilder.Entity<UnidadeMedida>()
+                .Property(um => um.Nome)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<UnidadeMedida>()
+                .Property(um => um.Abrebiacao)
+                .HasMaxLength(4)
+                .IsRequired();
         }
     }
 }
